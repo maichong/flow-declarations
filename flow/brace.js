78a1b611df102ede82c8a462ab5574925928458e
Liang Xingchen <liang@maichong.it>
2017-12-04 19:55:09 +0800

    brace

---
+++ flow/brace.js
@@ -0,0 +1,21 @@
+// TODO
+
+declare module 'brace/mode/dockerfile' {
+  declare var exports: any;
+}
+
+declare module 'brace/mode/sh' {
+  declare var exports: any;
+}
+
+declare module 'brace/mode/javascript' {
+  declare var exports: any;
+}
+
+declare module 'brace/theme/terminal' {
+  declare var exports: any;
+}
+
+declare module 'brace/theme/chrome' {
+  declare var exports: any;
+}
