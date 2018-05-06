cba2742f660b511c5235064927800588bd13b328
Liang Xingchen <liang@maichong.it>
2018-05-06 23:13:42 +0800

    0.4.5

--- socket.io/socket.io.js
+++ socket.io/socket.io.js
@@ -466,6 +466,82 @@ declare type SocketIO$Namespace = {
   compress(compress: boolean): SocketIO$Namespace
 }
 
+/**
+ * The base emiter class, used by Socket and Manager
+ */
+declare interface SocketIO$Emitter {
+  /**
+   * Adds a listener for a particular event. Calling multiple times will add
+   * multiple listeners
+   * @param event The event that we're listening for
+   * @param fn The function to call when we get the event. Parameters depend on the
+   * event in question
+   * @return This Emitter
+   */
+  on(event: string, fn: Function): SocketIO$Emitter;
+
+  /**
+   * @see on( event, fn )
+   */
+  addEventListener(event: string, fn: Function): SocketIO$Emitter;
+
+  /**
+   * Adds a listener for a particular event that will be invoked
+   * a single time before being automatically removed
+   * @param event The event that we're listening for
+   * @param fn The function to call when we get the event. Parameters depend on
+   * the event in question
+   * @return This Emitter
+   */
+  once(event: string, fn: Function): SocketIO$Emitter;
+
+  /**
+   * Removes a listener for a particular type of event. This will either
+   * remove a specific listener, or all listeners for this type of event
+   * @param event The event that we want to remove the listener of
+   * @param fn The function to remove, or null if we want to remove all functions
+   * @return This Emitter
+   */
+  off(event: string, fn?: Function): SocketIO$Emitter;
+
+  /**
+   * @see off( event, fn )
+   */
+  removeListener(event: string, fn?: Function): SocketIO$Emitter;
+
+  /**
+   * @see off( event, fn )
+   */
+  removeEventListener(event: string, fn?: Function): SocketIO$Emitter;
+
+  /**
+   * Removes all event listeners on this object
+   * @return This Emitter
+   */
+  removeAllListeners(): SocketIO$Emitter;
+
+  /**
+   * Emits 'event' with the given args
+   * @param event The event that we want to emit
+   * @param args Optional arguments to emit with the event
+   * @return Emitter
+   */
+  emit(event: string, ...args: any[]): SocketIO$Emitter;
+
+  /**
+   * Returns all the callbacks for a particular event
+   * @param event The event that we're looking for the callbacks of
+   * @return An array of callback Functions, or an empty array if we don't have any
+   */
+  listeners(event: string): Function[];
+
+  /**
+   * Returns if we have listeners for a particular event
+   * @param event The event that we want to check if we've listeners for
+   * @return True if we have listeners for this event, false otherwise
+   */
+  hasListeners(event: string): boolean;
+}
 
 /**
  * The socket, which handles our connection for a namespace. NOTE: while
@@ -473,7 +549,7 @@ declare type SocketIO$Namespace = {
 as we have a problem with the emit() event (as it's overridden with a
 different return)
 */
-declare type SocketIO$Socket = {
+declare interface SocketIO$Socket extends SocketIO$Emitter {
 
   /**
    * The namespace that this socket is for
